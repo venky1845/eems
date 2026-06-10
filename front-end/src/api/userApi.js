f1ac04cd5b8cdc1616ee7f91df6df4d897052e72
Your Name <you@example.com>
2026-06-10 14:47:21 +0530

    cached files

--- front-end/src/api/userApi.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({ baseURL: 'http://localhost:8000' })
-
-export const getUsers     = ()       => api.get('/api/users').then(r => r.data)
-export const signupUser   = (data)   => api.post('/api/users/signup', data).then(r => r.data)
-export const loginUser    = (data)   => api.post('/api/users/login', data).then(r => r.data)
-export const deactivateUser = (id)   => api.patch(`/api/users/${id}/deactivate`).then(r => r.data)
